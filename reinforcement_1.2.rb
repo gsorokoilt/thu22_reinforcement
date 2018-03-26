407cf4a8bdea19713213ae2568c52d70363c6142
GORDON SOROKOLIT <gordon.sorokolit@gmail.com>
2018-03-26 12:51:19 -0400

    add reinforcement

---
+++ reinforcement_1.2.rb
@@ -0,0 +1,17 @@
+train_sched = [
+  {train: "72C", frequency_in_minutes: 15, direction: "north"},
+  {train: "72D", frequency_in_minutes: 15, direction: "south"},
+  {train: "610", frequency_in_minutes: 5, direction: "north"},
+  {train: "611", frequency_in_minutes: 5, direction: "south"},
+  {train: "80A", frequency_in_minutes: 30, direction: "east"},
+  {train: "80B", frequency_in_minutes: 30, direction: "west"},
+  {train: "110", frequency_in_minutes: 15, direction: "north"},
+  {train: "111", frequency_in_minutes: 15, direction: "south"}
+]
+
+train_sched.each do |train|
+  if train[:direction] == "north"  # => "north"
+    puts train[:train] # if the direction is north
+    #how do i get diretion info from each hash
+  end
+end
